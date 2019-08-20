74d3b9a40549f44a63b3bc189ff3128e98e2ded6
Kermabon Stéphane <skermabon@talend.com>
2019-08-20 20:18:56 +0200

    Init Springboot project

---
+++ sb/backend/folder/src/main/java/org/talend/dataprep/sb/folder/FolderApplication.java
@@ -0,0 +1,13 @@
+package org.talend.dataprep.sb.folder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderApplication.class, args);
+	}
+
+}
---
+++ sb/backend/preparation/src/main/java/org/talend/dataprep/sb/preparation/PreparationApplication.java
@@ -0,0 +1,13 @@
+package org.talend.dataprep.sb.preparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparationApplication.class, args);
+	}
+
+}
