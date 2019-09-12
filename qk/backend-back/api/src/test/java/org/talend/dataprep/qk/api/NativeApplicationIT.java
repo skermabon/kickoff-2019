584664bfa6d2091ed0fe73d0cba48532b659a264
Kermabon Stéphane <skermabon@talend.com>
2019-09-12 18:23:47 +0200

    remove old code

--- qk/backend-back/api/src/test/java/org/talend/dataprep/qk/api/NativeApplicationIT.java
+++
@@ -1,9 +0,0 @@
-package org.talend.dataprep.qk.api;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeApplicationIT extends ApplicationTest {
-
-    // Execute the same tests but in native mode.
-}
--- qk/backend-back/preparation/src/test/java/org/talend/dataprep/qk/preparation/NativeApplicationIT.java
+++
@@ -1,9 +0,0 @@
-package org.talend.dataprep.qk.preparation;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeApplicationIT extends ApplicationTest {
-
-    // Execute the same tests but in native mode.
-}
